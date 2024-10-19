0bb4fe52f5367ee77aacc9a40558df6ec104ef9d
kalfaro2025 <kalfaro@zagmail.gonzaga.edu>
2024-10-18 20:39:17 -0700

    Converted webpage to three file system

---
+++ script.js
@@ -0,0 +1,110 @@
+$(document).ready(function(){
+            
+    var seconds = "00";
+    var tens = "00";
+    var $appendTens = $("#tens");//document.getElementById("tens")
+    var $appendSeconds = $("#seconds");//document.getElementById("seconds")
+    var $buttonStart =$('#button-start');//document.getElementById('button-start');
+    var $buttonStop = $('#button-stop');//document.getElementById('button-stop');
+    var $buttonReset = $('#button-reset');//document.getElementById('button-reset');
+    var interval;
+
+    $("#timer").addClass('timer-background'); 
+    setInterval(function(){
+        $(".timer-background").animate({
+            opacity: 1.0
+        }, 1000);
+        $(".timer-background").animate({
+            opacity: 0.8
+        }, 1000);
+    });
+    $("button").css({
+        marginTop: "15px",
+    });
+    $("button").addClass('timer-buttons');
+    $(".timer-buttons").hover(
+        function(){
+            $(".timer-buttons").css({
+                color: "green",
+                height: "50px",
+            });
+        },
+        function(){
+            $(".timer-buttons").css({
+                color: "black",
+                height: "40px",
+            });
+        });
+    $("#seconds").css({
+        fontFamily: "Chalkduster",
+        color: "white"
+    });
+    $("#tens").css({
+        fontFamily: "Chalkduster",
+        color: "DarkBlue"
+    });
+    $('div').css({
+        "background-image": "url('image copy.png')",
+        height: "auto",
+        width: "auto",
+        padding: "10px",
+        textAlign: "center",
+
+    });
+    $('body').css({
+        borderStyle: "dotted",
+        borderColor: "CornflowerBlue",
+        borderWidth: "5px"
+    });
+    $('h1').css({
+        color: "Purple",
+        fontFamily: "Chalkduster",
+        fontSize: "55px",
+        padding: "0 auto",
+        margin: "0 auto",
+    });
+    $buttonStart.click(function (){
+        clearInterval(interval);
+        interval = setInterval(startTimer, 10);
+        $('.timer-background').css("background-color", "rgb(51,165, 50)"); 
+    });
+    $buttonStop.click(function(){
+        clearInterval(interval);
+        if(seconds == "00" && tens =="00"){
+            $('.timer-background').css(backgroundColor, "grey");
+            $('.timer-background').css(opacity, "1");
+        }
+        else{
+            $('.timer-background').css("background-color", "red"); 
+            $('.timer-background').css(opacity, "1");
+        }
+    });
+    $buttonReset.click(function(){ 
+        clearInterval(interval);
+        tens = "00";
+        seconds = "00";
+        $appendTens.html(tens);
+        $appendSeconds.html(seconds);
+        $('.timer-background').css("background-color", "grey"); 
+        $('.timer-background').css(opacity, "1");
+    });
+    function startTimer() {
+        tens++;
+        if (tens < 9) {
+            $appendTens.html("0" + tens);
+        }
+        if (tens > 9) {
+            $appendTens.html(tens);
+        }
+        if (tens > 99) {
+            console.log("seconds");
+            seconds++;
+            $appendSeconds.html("0" + seconds);
+            tens = 0;
+            $("#appendTens").html("0" + 0);
+        }
+        if (seconds > 9) {
+            $appendSeconds.html(seconds);
+        }
+    }
+});
